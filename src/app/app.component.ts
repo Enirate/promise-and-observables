46b8b9b05393bd8c043f54487bd9fa14ee290b57
Enirate <enirate@gmail.com>
2017-01-07 10:49:18 +0100

    first instance of promise implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+items:Array<string>;
+urls:Array<string>;
+constructor(private wikipediaService:WikipediaService) {}
+
+search(term) {
+  this.wikipediaService.search(term)
+  .then(items => {this.items = items[1];
+    console.log(items)})
+ // .then(items => this.urls = items[2])
+  .catch(err => console.log(err));
+}
 }
